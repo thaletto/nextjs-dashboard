6e135ae63b25390ef288696efc030c29a273f42b
Laxman K R <krlaxman03@gmail.com>
2024-08-07 21:12:49 +0530

    Chapter 8

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,7 +11,10 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000))
     const data = await sql<Revenue>`SELECT * FROM revenue`;
+    console.log("Data fetch completed after 3 seconds.");
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
