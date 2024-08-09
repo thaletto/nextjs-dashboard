2a0b38a30234864ed251a8aad096fe67f0581d8e
Laxman K R <krlaxman03@gmail.com>
2024-08-09 13:09:37 +0530

    Chapter 10

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 import React from "react";
+export const experimental_ppr = true;
 
 export default function Layout({children}: {children: React.ReactNode}) {
     return (
