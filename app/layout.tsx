f006c142a7d05fad7cd9d162be4c73f58fbd512e
Laxman K R <krlaxman03@gmail.com>
2024-08-09 16:07:20 +0530

    Chapter 16 B

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Acme Dashboard',
   description: 'The official Next.js Course Dashboard, built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh');
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
 };
 
 export default function RootLayout({
